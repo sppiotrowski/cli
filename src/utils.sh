d967723a79df6cf75e813ebfaceaf748f000f2df
Sebastian Piotrowski <sebastian.piotrowski@outfittery.de>
2018-11-17 17:36:35 +0100

    backup:

--- src/utils.sh
+++ src/utils.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
 
-alias .util.date='echo $(date +%Y.%m.%d) | tee /dev/tty | pbcopy'
-alias .yay='echo "Yippee-Ki-Yay…" | tee /dev/tty | pbcopy'
+.util.date() {
+  date +%Y.%m.%d | tee /dev/tty | pbcopy
+}
 
 .util.email() {
   echo "spp.$(date +%Y.%m.%d.%H.%M)@test.de" | tee /dev/tty | pbcopy
 }
+
 .util.replace() {
   KEY="$1"; shift
   VAL="$1"; shift
