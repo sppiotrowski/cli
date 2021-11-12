9364532a970e965f44112cafd178759980ecb97b
Sebastian Piotrowski <sebastian.piotrowski@outfittery.de>
2021-11-12 09:36:11 +0100

    backup:

--- src/git.sh
+++ src/git.sh
@@ -1,47 +1,65 @@
 #!/usr/bin/env bash
 
+.git.project_name() {
+  local func_name="${FUNCNAME[0]}"
+  __spp_stat "$func_name"
+
+  git config --local --get remote.origin.url | sed -n 's/^.*\/\(.*\)\.git$/\1/p'
+}
+
 .git.current() {
+  local func_name="${FUNCNAME[0]}"
+  __spp_stat "$func_name"
+
   git rev-parse --abbrev-ref HEAD
 }
 
 .git.upstream() {
+  local func_name="${FUNCNAME[0]}"
+  __spp_stat "$func_name"
+
   git push --set-upstream origin "$(.git.current)"
 }
 
 .git.current.jira() {
-    .git.current | grep -o '^[A-Z]\+-[0-9]\+'
+  local func_name="${FUNCNAME[0]}"
+  __spp_stat "$func_name"
+
+  .git.current | grep -o '^[A-Z]\+-[0-9]\+'
 }
 
 .git.ci() {
+  local func_name="${FUNCNAME[0]}"
+  __spp_stat "$func_name"
+
   DEFAULT="$(.git.project_name)"
   APP_NAME="${1:-"${DEFAULT}"}"
   open "http://ci-1.dev.outfittery.de/search/?q=$APP_NAME"
 }
 
 .git.prune() {
+  local func_name="${FUNCNAME[0]}"
+  __spp_stat "$func_name"
+
   git fetch -p
   for branch in $(git branch | grep -v master | grep -v -e '^* '); do
     git branch -D "$branch"
   done
 }
 
-_git_config_url() {
-  git config --local remote.origin.url
-}
-
-_git_url() {
-  _git_config_url | awk -F@ '{print $2}' | sed -e 's/.git//' -e 's/:/\//'
+__spp_git_url() {
+  # git config --local remote.origin.url | awk -F@ '{print $2}' | sed -e 's/.git//' -e 's/:/\//'
+  git config --local --get remote.origin.url | sed -n 's/^git@\(.*\)\.git$/\1/p'
 }
 
 .git.pull_request() {
-  open "https://$(_git_url)/pull/new/$(.git.current)"
-}
+  local func_name="${FUNCNAME[0]}"
+  __spp_stat "$func_name"
 
-.git.project_name() {
-  _git_url | awk -F'/' '{ print $3 }'
+  open "https://$(__spp_git_url)/pull/new/$(.git.current)"
 }
 
 .git.open() {
-  open "https://$(_git_url)"
+  open "https://$(__spp_git_url)"
 }
 
--- src/jenkins.sh
+++ src/jenkins.sh
@@ -4,24 +4,25 @@
 # echo 'SPP_JENKINS_USER=me:my-api-token' >> ~/.bashrc
 # echo 'SPP_JENKINS_BASE_URL=https://example.jenkins.com' >> ~/.bashrc
 
-# Jira API docs
+# Jenkins API docs
 # https://www.jenkins.io/doc/book/using/remote-access-api/
 
-.jenkins.job() {
+.jenkins.build() {
   local func_name="${FUNCNAME[0]}"
   __spp_stat "$func_name"
+
   if [[ $1 = '-h' ]]; then
-    echo "usage: $func_name [-h] git_project_name git_branch_name"
+    echo "usage: $func_name [-h] git_project git_branch"
     return 1
   fi
 
-  local git_project="$1"
+  local git_project="${1-$(.git.project_name)}"
   if [[ -z $git_project ]]; then
     echo "$func_name" git_project is missing
     return 1
   fi
 
-  local git_branch="$2"
+  local git_branch="${2-$(.git.current)}"
   if [[ -z $git_branch ]]; then
     echo "$func_name" git_branch is missing
     return 1
@@ -34,3 +35,19 @@
     --url "${SPP_JENKINS_BASE_URL}/job/docker%20builds/job/${job_name}/buildWithParameters" \
     --data branch="$git_branch"
 }
+alias .jb=.jenkins.build
+
+.jenkins.open() {
+  local func_name="${FUNCNAME[0]}"
+  __spp_stat "$func_name"
+
+  if [[ $1 = '-h' ]]; then
+    echo "usage: $func_name [-h] git_project"
+    return 1
+  fi
+
+  local git_project="${1-$(.git.project_name)}"
+
+  open "${SPP_JENKINS_BASE_URL}/job/docker%20builds/job/build%20${git_project}%20image/"
+}
+alias .jo=.jenkins.open
